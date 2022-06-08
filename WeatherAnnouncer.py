9406423f56eca03a9e4d59395cd8c9d1dda35da6
Will Daknis <68512532+willrd3@users.noreply.github.com>
2022-06-08 14:20:21 -0600

    Update WeatherAnnouncer.py

--- WeatherAnnouncer.py
+++ WeatherAnnouncer.py
@@ -11,19 +11,19 @@ def temperatureClassification(temp):
     return 'COLD'
 
 
-#if hot and clear and not windy: 'Wear summer clothes today. 
-print('TESTING', weatherAnnouncer(77, True, False))
-# if hot and not clear and windy: 'Wear summer clothes today, but bring a rain jacket just in case.'
-print('TESTING', weatherAnnouncer(95, False, True))
-# if hot and not clear and not windy: 'Wear summer clothes today, but bring an umbrella just in case.'
-print('TESTING', weatherAnnouncer(95, False, False))
+# #if hot and clear and not windy: 'Wear summer clothes today. 
+# print('TESTING', weatherAnnouncer(77, True, False))
+# # if hot and not clear and windy: 'Wear summer clothes today, but bring a rain jacket just in case.'
+# print('TESTING', weatherAnnouncer(95, False, True))
+# # if hot and not clear and not windy: 'Wear summer clothes today, but bring an umbrella just in case.'
+# print('TESTING', weatherAnnouncer(95, False, False))
 
-# if fair AND clear AND not windy: 'Wear whatever you would like today.'
-print('TESTING', weatherAnnouncer(56, True, False))
-# if fair AND otherwise: 'Wear a jacket today.'
-print('TESTING', weatherAnnouncer(56, False, False))
+# # if fair AND clear AND not windy: 'Wear whatever you would like today.'
+# print('TESTING', weatherAnnouncer(56, True, False))
+# # if fair AND otherwise: 'Wear a jacket today.'
+# print('TESTING', weatherAnnouncer(56, False, False))
 
-# if cold AND windy AND PC: 'Just stay inside today.'
-print('TESTING', weatherAnnouncer(22, False, True))
-# if cold AND otherwise: 'Wear winter gear today.'
-print('TESTING', weatherAnnouncer(2, True, True))
+# # if cold AND windy AND PC: 'Just stay inside today.'
+# print('TESTING', weatherAnnouncer(22, False, True))
+# # if cold AND otherwise: 'Wear winter gear today.'
+# print('TESTING', weatherAnnouncer(2, True, True))
