bbc859fa4adac70c0af50a625c71020d02b7bebc
Will Daknis <daknis@rams.colostate.edu>
2022-06-08 13:37:18 -0600

    student starting point for weatherAnnouncer

--- WeatherAnnouncer.py
+++ WeatherAnnouncer.py
@@ -1,69 +1,29 @@
-# conditions: 
-#   temp (hot, fair, cold)
-#   windy or not windy
-#   sky conditions (persperating or cloudy, clear skies)
-#
-#   temp is a number <40 is cold, <75 is fair, else is hot
-#
-#
-#   hot, windy, clear:          summer clothes
-#   hot, not windy, clear:      summer clothes
-#   
-#   hot, windy, pc:             summer clothes, rain jacket
-#   hot, not windy, pc:         summer clothes, umbrella
-#   
-#   fair, windy, clear:       wear whatever
-#   fair, not windy, clear:   wear whatever
-#   
-#   fair, windy, pc:          wear whatever, rain jacket
-#   fair, not windy, pc:      wear whatever, umbrella
-#
-#   cold, windy, clear:         winter clothes needed
-#   cold, not windy, clear:     winter clothes needed
-#   
-#   cold, windy, pc:            just stay inside
-#   cold, not windy, pc:        winter clothes needed
-
-
-# if hot AND clear: summer clothers
-# if hot and pc: summer clothes
-        # if/ else for rain jacket/umbrella
-# if fair AND clear: wear whatever
-# if fair AND PC: wear whatever
-        #if/ else for rain jacket/umbrella
-# if cold:
-        #AND windy AND PC: just stay inside
-        #else: winter gear needed
-
-def weatherAnnouncement(temp, windy, clearSkies):
-    recommendation = ''
+def weatherAnnouncer(temp, clearSkies, windy):
+    #try to combine these two statements into one if statement!
     tempClass = temperatureClassification(temp)
-    if(tempClass == 'Hot' and clearSkies):
-        return 'Wear summer clothes today'
-        #1/2 possible ends of included code for students for this function
-    if(tempClass == 'Hot' and not clearSkies):
-        gear = rainGearCheck(windy)
-        return 'Wear summer clothes today, but ' + gear
-        #2/2 possible ends
+    if(tempClass == 'Hot' and clearSkies and not windy):
+        return 'Wear summer clothes today.'
+    if(tempClass == 'Hot' and clearSkies and windy):
+        return 'Wear summer clothes today.'
 
 
-#This can be deleted if lab is determined to be too difficult
-#Can serve as first test in zybooks if kept
 def temperatureClassification(temp):
-    if(temp < 40):
-        return 'Cold'
-    elif(temp < 75):
-        return 'Fair'
-    else:
-        return 'Hot'
+    return 'Cold'
 
-#optional helper method
-def rainGearCheck(windy):
-    if(windy):
-        return 'rain jacket'
-    else:
-        return 'umbrella'
 
-def main():
-    print('TESTING', weatherAnnouncement(77, False,True))
-    #make a real python main
+#if hot and clear and not windy: 'Wear summer clothes today. 
+print('TESTING', weatherAnnouncer(77, True, False))
+# if hot and not clear and windy: 'Wear summer clothes today, but bring a rain jacket just in case.'
+print('TESTING', weatherAnnouncer(95, False, True))
+# if hot and not clear and not windy: 'Wear summer clothes today, but bring an umbrella just in case.'
+print('TESTING', weatherAnnouncer(95, False, False))
+
+# if fair AND clear AND not windy: 'Wear whatever you would like today.'
+print('TESTING', weatherAnnouncer(56, True, False))
+# if fair AND otherwise: 'Wear a jacket today.'
+print('TESTING', weatherAnnouncer(56, False, False))
+
+# if cold AND windy AND PC: 'Just stay inside today.'
+print('TESTING', weatherAnnouncer(22, False, True))
+# if cold AND otherwise: 'Wear winter gear today.'
+print('TESTING', weatherAnnouncer(2, True, True))
